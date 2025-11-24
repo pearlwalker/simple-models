789fc8ccaaf3fa943b578f141efb907c5cc32b96
pearlwalker <paw7293@rit.edu>
2025-11-24 14:47:49 -0500

    Added name key + values I followed the format CatSchema in Cat.js used for its name property

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 let DogModel = {};
 
 const DogSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
 });
 
 DogModel = mongoose.model('Dog', DogSchema);
