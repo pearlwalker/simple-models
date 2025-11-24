7ed1c12da3a9f01c2ae16e5aaa587bd6624b9962
pearlwalker <paw7293@rit.edu>
2025-11-24 14:50:17 -0500

    Added createdDate key + values

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -19,6 +19,10 @@ const DogSchema = new mongoose.Schema({
     required: true,
     min: 0,
   },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 DogModel = mongoose.model('Dog', DogSchema);
