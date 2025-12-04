13e4717bee43a5816e6a2ba862d0ea4c9e43dc35
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-03 20:11:20 -0500

    Reverted back to POST

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -153,7 +153,7 @@ const createDog = async (req, res) => {
 };
 
 const ageDog = async (req, res) => {
-  const updatePromise = Dog.findOneAndUpdate({ name: req.query.searchDog }, { $inc: { age: 1 } }, {
+  const updatePromise = Dog.findOneAndUpdate({  }, { $inc: { age: 1 } }, {
     returnDocument: 'after',
     sort: { createdDate: 'descending' },
   }).lean().exec();
